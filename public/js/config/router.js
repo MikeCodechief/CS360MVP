d5b3da9e83a68b8ab7499aa45bb15830f41e9e4d
Jeffrey Young <jeffreeyoung@yahoo.com>
2014-12-02 18:41:34 -0700

    adding invalid route page

--- public/js/config/router.js
+++ public/js/config/router.js
@@ -1,12 +1,15 @@
 airQualityApp.config(['$routeProvider', '$locationProvider',
   function ($routeProvider, $locationProvider) {
-        $routeProvider.
-        when('/locations', {
-            templateUrl: 'views/locations.html',
+        $routeProvider
+        .when('/locations', {
+            templateUrl: 'views/locations.html'
         })
         .when('/', {
             templateUrl: 'views/home.html'
         })
+        .otherwise({
+            templateUrl: 'views/invalid-route.html'
+        });
         // use the HTML5 History API
         $locationProvider.html5Mode(true);
 }]);
