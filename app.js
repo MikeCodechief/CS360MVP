09f7e7d55e705976a4f7b1041e2a7b3c86205243
Jeffrey Young <jeffreeyoung@yahoo.com>
2014-12-04 01:10:25 -0700

    improved spacing on app.js

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ var logger = require('morgan');
 
 var app = express()
 
+
 //serve static html files
 app.use(express.static(__dirname + '/public'))
+
 
 //log requests
 app.use(logger('dev'));
 
+
 //attach routers to app
 var apiRouters = require('./routes/apiRouters')
+var indexRouter = require('./routes/indexRouter')
+
 app.use('/api/airdata/', apiRouters.airData )
 app.use('/api/user/', apiRouters.user )
-app.use('/', require('./routes/indexRouter'))
+app.use('/', indexRouter)
+
 
 var port = process.env.PORT || 1234
 app.listen(port, function(){
