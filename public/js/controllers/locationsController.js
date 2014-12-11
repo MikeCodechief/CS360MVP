3bb16162bfe17ceb7821c935139088661ac58a96
Jeffrey Young <jeffreeyoung@yahoo.com>
2014-12-11 02:42:18 -0700

    adding data persistance stuff, nothing to do with airdata factory

--- public/js/controllers/locationsController.js
+++ public/js/controllers/locationsController.js
@@ -3,33 +3,62 @@ airQualityApp.controller("LocationsController", ['$scope', '$http',
 
         $scope.locations = [];
 
-        console.log("locatins controller")
+        $http.get('/api/user/locations').success(function (locations) {
+            console.log(locations)
+            for (var i = 0; i < locations.length; i++) {
+                var location = locations[i]
+                var long = location.longitude
+                var lat = location.latitude
+                var url = "/api/airdata/coordinates?long=" + long + "&lat=" + lat;
+                $http.get(url)
+                    .success(function (data) {
+                        var tmp = {}
+                        tmp.name = data[0].ReportingArea + ", " + data[0].StateCode
+                        tmp.quality = data[0].Category.Name
+                        tmp.longitude = data[0].Longitude
+                        tmp.latitude = data[0].Latitude
+                        $scope.locations.push(tmp)
+                    })
+            }
+        })
+
 
         $scope.add = function () {
             if ($scope.newLocation) {
-                var url = "/api/airdata/zipcode/" + $scope.newLocation
+                var url = "/api/user/add/zipcode/" + $scope.newLocation
                 $http.get(url)
-                    .success(function(data){
+                    .success(function (data) {
+                        if (!data[0]) {
+                            alert('no data for that url')
+                            return;
+                        }
+
                         var name = data[0].ReportingArea + ", " + data[0].StateCode
                         var quality = data[0].Category.Name
-                        
+                        var long = data[0].Longitude
+                        var lat = data[0].Latitude
+
                         var tmp = {
                             name: name,
-                            quality: quality
+                            quality: quality,
+                            longitude: long,
+                            latitude: lat
                         }
                         $scope.locations.push(tmp)
-                        
-                })
+                    })
             }
             $scope.newLocation = "";
-            
+
         }
 
-        $scope.add(3455)
-        $scope.add(234234)
-
         $scope.remove = function (i) {
-            $scope.locations.splice(i, 1);
+            var location = $scope.locations[i]
+            var url = "/api/user/locations?long=" + location.longitude + "&lat=" + location.latitude;
+            
+            $http.delete(url).success(function(data){
+                console.log(data)
+                $scope.locations.splice(i, 1)
+            })
         }
 
 
--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -1,5 +1,8 @@
 var express = require('express')
+var request = require('request')
+var airNowUrlBuilder = require('./../../services/airNowUrlBuilder')
 var ensureAuthenticated = require('./../../services/ensureAuthenticated')
+var Location = require('./../../models/location')
 
 var router = express.Router()
 
@@ -9,14 +12,84 @@ router.get('/', function(req, res){
     res.json(req.user)
 })
 
+
+router.use(ensureAuthenticated)
+
+router.get('/add/coordinates', function (req, res) {
+    var longitude = req.query.long
+    var latitutde = req.query.lat
+
+    var airNowUrl = airNowUrlBuilder.latlong(latitutde, longitude)
+    request.get({
+        url: airNowUrl,
+        json: true
+    }, function (e, r, data) {
+        if (data[0]) {
+            var long = data[0].Longitude
+            var lat = data[0].Latitude
+            
+            var newLocation = new Location()
+            newLocation.longitude = long
+            newLocation.latitude = lat
+            newLocation.user_id = req.user._id
+            newLocation.save(function(err, loc){
+            if (err) { return res.json(err) }
+                data[0]._id = loc._id
+                return res.json(loc, data)
+            })
+        } else {
+            return res.json(data)
+        }
+    })
+})
+
+router.get("/add/zipcode/:zipcode", function (req, res) {
+    var airNowUrl = airNowUrlBuilder.zipcode(req.params.zipcode)
+    request.get({
+        url: airNowUrl,
+        json: true
+    }, function (e, r, data) {
+        if (data[0]){
+            var long = data[0].Longitude
+            var lat = data[0].Latitude
+            
+            var newLocation = new Location()
+            newLocation.longitude = long
+            newLocation.latitude = lat
+            newLocation.user_id = req.user._id
+            newLocation.save(function(err, loc){
+            if (err) { return res.json(err) }
+                data[0]._id = loc._id
+                return res.json(loc, data)
+            })
+        } else {
+            return res.json(data)  
+        }
+    })
+})
+
 router.get('/locations', function(req,res){
-    res.send('user locations')
+    Location.find({ user_id: req.user._id }, function(err, locations){
+        if (err){
+            res.json(err)   
+        }
+        else {
+            res.json(locations)   
+        }
+    })
 })
 
-router.post('/locations', function(req,res){
-  res.send('location added')  
-})
-
+router.delete('/locations', function(req,res){
+    Location.findOneAndRemove({ longitude: req.query.long, latitude: req.query.lat }, function (err, data) {
+        if (err) { 
+            return res.json(err)
+        }
+        else {
+            return res.json({message: "successful removal", data: data})   
+        }
+        
+    });
+});
 
 
 module.exports = router
